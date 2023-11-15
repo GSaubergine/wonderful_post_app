809ad49544f99d4067e9184564a41f0f00e58e2d
川瀬紳吾 <>
2023-11-15 12:44:00 +0900

    マイページの作成とsample_articleの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
-  resources :sample_articles
+  get '/mypage', to: 'mypage#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
 end

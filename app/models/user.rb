e85f481d49b5e16a71e7a5985ec6000a212e75f7
川瀬紳吾 <>
2023-11-24 19:37:47 +0900

    no message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :tag_articles, dependent: :destroy
-  has_many :articles, through: :tag_articles
+
+  has_many :articles
 end
